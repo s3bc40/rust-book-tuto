94ea4adefed2adaefe34a98f97afa669e151b4f2
s3bc40 <s3bc40@gmail.com>
2025-03-13 11:35:40 +0100

    feat: result handling ok, err amd propagating err
    Next todo: A shortcut for propagating errors: ? operator

--- rust-err-handling/src/main.rs
+++ rust-err-handling/src/main.rs
@@ -1,4 +1,5 @@
-use std::fs::File;
+use std::io::{self, Read};
+use std::{fs::File, io::ErrorKind};
 
 fn main() {
     // panic!("crash and burn");
@@ -8,6 +9,50 @@ fn main() {
     let greeting_file_result = File::open("hello.txt");
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {error:?}"),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {e:?}"),
+            },
+            other_error => panic!("Problem opening the file: {other_error:?}"),
+        },
     };
+
+    // Shortcuts for Panic on Error: unwrap and expect
+    let f = File::open("hello.txt").unwrap();
+    // Fav Rustaceans use expect
+    let f = File::open("hello.txt").expect("hello.txt is missing you should include it");
+
+    // Propagating Errors
+    let username = read_username_from_file();
 }
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+// Another way to do this with closure chapt 13
+// use std::fs::File;
+// use std::io::ErrorKind;
+
+// fn main() {
+//     let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+//         if error.kind() == ErrorKind::NotFound {
+//             File::create("hello.txt").unwrap_or_else(|error| {
+//                 panic!("Problem creating the file: {error:?}");
+//             })
+//         } else {
+//             panic!("Problem opening the file: {error:?}");
+//         }
+//     });
+// }
