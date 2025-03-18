5cdd18d16a2c4b4e992d27de0622482f9d6b2501
s3bc40 <s3bc40@gmail.com>
2025-03-18 11:04:16 +0100

    feat: io args and file with refactoring for lib rs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,40 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    // Reading args
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+
+    // Saving args
+    let config = Config::new(&args);
+
+    println!("Searchin for `{}`", config.query);
+    println!("In file `{}`", config.file_path);
+
+    // Reading a file
+    let content =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    println!("With text:\n```\n{}\n```", content);
+}
+
+// Grouping configuration values
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        // Refactored code
+        // Error handling
+        if args.len() < 3 {
+            panic!("Not enough arguments: 2 expected")
+        }
+        // @dev Many Rustaceans avoid using clone to fix ownership problems because of its runtime cost
+        // Gain simplicity other perf (fine here)
+        let query = args[1].clone();
+        let file_path = args.get(2).expect("No file path in args").clone();
+
+        Config { query, file_path }
+    }
 }
